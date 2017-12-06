ab061ea986427358c6355d57dcbe445e8312f952
tsmckay <trevormckayhb@gmail.com>
2017-12-05 20:37:15 -0800

    added barebones sketches

---
+++ sketches/arduino_3/arduino_3.ino
@@ -0,0 +1,14 @@
+/* Features to add
+ * Pull data from server
+ * Control respective module
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ sketches/arduino_main/arduino_main.ino
@@ -0,0 +1,15 @@
+/* Features to add:
+ * Collection of data from sensors
+ * Ability to send data to server over network
+ * Ability to read data over network
+ */
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
